3937664a8ebbea048cf2e4265466ea55051ac70a
Huy Anh <huyanh2504@gmail.com>
2025-10-23 18:27:52 +0200

    add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="youtube-sentiment-insights",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Huy Anh",
+    author_email="io.tommy.dang@gmail.com",
+    install_requires=[],
+)
